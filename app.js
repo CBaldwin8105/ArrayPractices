9057b1e30a3aa6581cee72632d5edfb6ff1a683e
Baldwin <cbaldwin@FVFZMYCQJ1WK.tld>
2022-02-08 14:33:13 -0600

    practice1

---
+++ app.js
@@ -0,0 +1,17 @@
+var friends = ["Riker", "Jesse", "Emily", "Bucko", "Nice", "Jonah", "Gabriel", "Loel", "Ella", "Austin"];
+
+console.log(friends);
+
+console.log(friends.sort());
+
+console.log(friends.toString())
+
+friends[0] = "Lawrence";
+
+friends[5] = "Sammy";
+
+friends[7] = "Aaron";
+
+console.log(friends);
+
+console.log(friends.length);
